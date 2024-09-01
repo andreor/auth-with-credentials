778d7907f41c2551899cb7a826824d556810d091
André Ribeiro <andreor@gmail.com>
2024-08-31 21:49:25 -0300

    Update LoginForm.jsx

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -57,7 +57,6 @@ export default function LoginForm() {
               {error}
             </div>
           )}
-
           <Link className="text-sm mt-3 text-right" href={"/register"}>
             account? <span className="underline">Register</span>
           </Link>
